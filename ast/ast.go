b1d13ecbcec603eddee72528782943550ef43b00
Sam Rosenthal <samrrosenthal@gmail.com>
2023-04-22 16:17:32 -0400

    finished up to 2.5

---
+++ ast/ast.go
@@ -0,0 +1,51 @@
+package ast
+
+import "monkey/token"
+
+// Types
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Program
+type Program struct {
+	Statements []Statement
+}
+
+func (program *Program) TokenLiteral() string {
+	if len(program.Statements) > 0 {
+		return program.Statements[0].TokenLiteral()
+	}
+
+	return ""
+}
+
+// Let Statement
+type LetStatement struct {
+  Statement
+	Token token.Token // token.LET
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+// Identifier
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
---
+++ parser/parser.go
@@ -0,0 +1,118 @@
+package parser
+
+import (
+	"fmt"
+	"monkey/ast"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+type Parser struct {
+	lexer     *lexer.Lexer
+	curToken  token.Token
+	peekToken token.Token
+	errors    []string
+}
+
+func New(l *lexer.Lexer) *Parser {
+	parser := &Parser{
+		lexer:  l,
+		errors: []string{},
+	}
+
+	parser.nextToken()
+	parser.nextToken()
+
+	return parser
+}
+
+func (parser *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{
+		Statements: []ast.Statement{},
+	}
+
+	for !parser.curTokenIs(token.EOF) {
+		stmt := parser.parseStatement()
+
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+
+		parser.nextToken()
+	}
+
+	return program
+}
+
+func (parser *Parser) Errors() []string {
+	return parser.errors
+}
+
+// // // // // // // // // // // // // // // // // // //
+// // // // // // // // Private // // // // // // // //
+// // // // // // // // // // // // // // // // // //
+
+func (parser *Parser) parseStatement() ast.Statement {
+	switch parser.curToken.Type {
+	case token.LET:
+		return parser.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (parser *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{
+		Token: parser.curToken,
+	}
+
+	if !parser.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{
+		Token: parser.curToken,
+		Value: parser.curToken.Literal,
+	}
+
+	if !parser.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: Skip expressions by finding the semi
+	// until we implement this part
+	for !parser.curTokenIs(token.SEMICOLON) {
+		parser.nextToken()
+	}
+
+	return stmt
+}
+
+func (parser *Parser) curTokenIs(t token.TokenType) bool {
+	return parser.curToken.Type == t
+}
+
+func (parser *Parser) expectPeek(t token.TokenType) bool {
+	if parser.peekTokenIs(t) {
+		parser.nextToken()
+		return true
+	}
+
+  parser.peekError(t)
+	return false
+}
+
+func (parser *Parser) peekTokenIs(t token.TokenType) bool {
+	return parser.peekToken.Type == t
+}
+
+func (parser *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, parser.peekToken.Type)
+	parser.errors = append(parser.errors, msg)
+}
+
+func (parser *Parser) nextToken() {
+	parser.curToken = parser.peekToken
+	parser.peekToken = parser.lexer.NextToken()
+}
